5ffe4f30cd12d2d9faa8aae4f60e4556b590a577
Иброхим <ibrokhimshokirovs@gmail.com>
2025-09-22 20:01:58 +0500

    Create homework.py

---
+++ lessom-1/homework/homework.py
@@ -0,0 +1,23 @@
+side = float(input("Enter side of square: "))
+perimeter = 4 * side
+area = side ** 2
+print("Perimeter =", perimeter)
+print("Area =", area)
+
+import math
+
+diameter = float(input("Enter diameter of circle: "))
+length = math.pi * diameter
+print("Length of circle =", length)
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+mean = (a + b) / 2
+print("Mean =", mean)
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+print("Sum =", a + b)
+print("Product =", a * b)
+print("Square of a =", a ** 2)
+print("Square of b =", b ** 2)
